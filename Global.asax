40a5e96e93ab423c75e8f672c5076d1b6efa5dd7
vepakshi <v3pakshir3ddy52@hotmail.com>
2017-05-27 19:14:10 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="binaryfactory.azuread1.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(binaryfactory.azuread1.Startup))]
+
+namespace binaryfactory.azuread1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigureAuth(app);
+        }
+    }
+}
